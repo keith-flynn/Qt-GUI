0221b13168817ef12a8d08a65821a037b954f89d
Keith Flynn <flynn.keith@gmail.com>
2022-12-10 12:16:16 -0500

    version 2, learning the logic

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+"""
+# Version 1
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 import sys
@@ -14,5 +16,25 @@ button.setText("Press Me")
 
 window.setCentralWidget(button)
 
+window.show()
+app.exec()
+"""
+
+# Version 2
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class ButtonHolder(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Button Holder")
+        button = QPushButton("Press Me")
+
+        # Set up button as central widget
+        self.setCentralWidget(button)
+
+app = QApplication(sys.argv)
+
+window = ButtonHolder()
 window.show()
 app.exec()
